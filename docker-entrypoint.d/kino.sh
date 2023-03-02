65f20b2e39a87eb775a5b1ff209f1f739de08e6f
Christopher Hornberger <christopher.hornberger@camptocamp.com>
2023-03-02 14:33:09 +0100

    fix: html

--- docker-entrypoint.d/kino.sh
+++ docker-entrypoint.d/kino.sh
@@ -2,13 +2,33 @@
 #little fun for my website, CHO
 
 fortunex() {
-  INDEX=/usr/local/apache2/htdocs/index.html
-  /usr/games/fortune -s | /usr/games/cowsay -s -f $(shuf -n 1 -e $(/usr/games/cowsay -l)) > $INDEX
+  /usr/games/fortune -s | /usr/games/cowsay -s -f $(shuf -n 1 -e $(/usr/games/cowsay -l)) >> "$INDEX"
   [ $? -ne 0 ] && fortunex
 }
 
+html_front() {
+  INDEX=/usr/local/apache2/htdocs/index.html
+  cat <<EOF > "$INDEX"
+    <html>
+    <head>
+    <meta charset="utf-8">
+    </head>
+    <title>have a fortune cookie ...</title>
+    <pre>
+EOF
+}
+
+html_end() {
+  cat <<EOF >> "$INDEX"
+  </pre>
+  </html>
+EOF
+}
+
 main() {
+  html_front
   fortunex
+  html_end
 }
 
 main "$@" || exit 1
