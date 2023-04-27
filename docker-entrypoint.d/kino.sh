74f610209bc734e0c3f2c75280462b580a8afacc
Christopher Hornberger <christopher.hornberger@camptocamp.com>
2023-04-27 08:42:43 +0200

    chore: clean code

--- docker-entrypoint.d/kino.sh
+++ docker-entrypoint.d/kino.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #little fun for my website, CHO
 
+write_to_index() {
+  INDEX=/usr/local/apache2/htdocs/index.html
+  tee -a "$INDEX"
+}
+
 fortunex() {
-  /usr/games/fortune -s | /usr/games/cowsay -s -f $(shuf -n 1 -e $(/usr/games/cowsay -l)) >> "$INDEX"
+  /usr/games/fortune -s | /usr/games/cowsay -s -f $(shuf -n 1 -e $(/usr/games/cowsay -l))
   [ $? -ne 0 ] && fortunex
 }
 
 html_front() {
-  INDEX=/usr/local/apache2/htdocs/index.html
-  cat <<EOF > "$INDEX"
+  cat <<EOF
 <html>
 <head>
 <meta charset="utf-8">
@@ -19,16 +23,16 @@ EOF
 }
 
 html_end() {
-  cat <<EOF >> "$INDEX"
+  cat <<EOF
 </pre>
 </html>
 EOF
 }
 
 main() {
-  html_front
-  fortunex
-  html_end
+  html_front | write_to_index
+  fortunex | write_to_index
+  html_end | write_to_index
 }
 
 main "$@" || exit 1
